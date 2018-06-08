8ab3d603752e8bb1090e0b4e438a72944bdd641b
BatyaGG <b.saduanov@gmail.com>
2018-06-08 07:45:55 +0000

    best model

--- MotorController.py
+++ MotorController.py
@@ -33,44 +33,44 @@ class MotorController:
         self.back_motor[1].off()
         
     def steer_left(self):
+        print('steer left')
         self.turn_in_process = False
         time.sleep(0.01)
         self.turn_in_process = True
         self.steer_motor[0].on()
         self.steer_motor[1].off()
-        self._wait_camera(30)
+        print(self._wait_camera(30))
         self.steer_motor[0].off()
         self.steer_motor[1].off()
-        self.turn_in_process = False
         
     def steer_right(self):
+        print('steer right')
         self.turn_in_process = False
         time.sleep(0.01)
         self.turn_in_process = True
         self.steer_motor[0].off()
         self.steer_motor[1].on()
-        self._wait_camera(120)
+        print(self._wait_camera(120))
         self.steer_motor[0].off()
         self.steer_motor[1].off()
-        self.turn_in_process = False
 
     def steer_middle(self):
+        print('steer middle')
         self.turn_in_process = False
         time.sleep(0.01)
         self.turn_in_process = True
-        print('started')
         if self._get_reading() < 80:
+            print('->')
             self.steer_motor[0].off()
             self.steer_motor[1].on()
-            self._wait_camera(79)
-            print('1 waited')
+            print(self._wait_camera(79))
             self.steer_motor[0].off()
             self.steer_motor[1].off()
         else:
+            print('<-')
             self.steer_motor[0].on()
             self.steer_motor[1].off()
-            self._wait_camera(88)
-            print('2 waited')
+            print(self._wait_camera(88))
             self.steer_motor[0].off()
             self.steer_motor[1].off()
         self.turn_in_process = False
@@ -82,13 +82,17 @@ class MotorController:
         self.back_motor[1].off()
 
     def _get_reading(self):
-        frame = self.camera.get_current_frame()
-        gamma = gamma_correction(frame, 2)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 2)
-        indexes = np.where(thresh.squeeze() == 0)[0]
-        reading = int(np.median(indexes))
-        return reading
+        median_reading = []
+        for i in range(3):
+            frame = self.camera.get_current_frame()
+            gamma = gamma_correction(frame, 2)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 2)
+            indexes = np.where(thresh.squeeze() == 0)[0]
+            reading = int(np.median(indexes))
+            median_reading.append(reading)
+        median_reading = np.median(median_reading)
+        return int(median_reading)
 
     def _wait_camera(self, until, tolerance=10):
         while self.turn_in_process:
@@ -100,9 +104,8 @@ class MotorController:
             cv2.waitKey(1)
             indexes = np.where(thresh.squeeze() == 0)[0]
             reading = int(np.median(indexes))
-            print(reading)
             if math.fabs(reading - until) <= tolerance:
-                return
+                return math.fabs(reading - until)
             
     def _stripe_to_show(self, stripe):
         stripe = stripe.reshape((1, -1))
